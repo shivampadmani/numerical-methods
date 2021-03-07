ea097f2741dbc527034c4b3b06255f112ae44ca6
shivampadmani <padmanishivamb@gmail.com>
2021-03-07 13:35:23 +0530

    GRG method

--- generalized-reduced-problem-method.cpp
+++ generalized-reduced-problem-method.cpp
@@ -2,38 +2,62 @@
 // Here we will primarily make code for non linear function with two variables only.Later it will be optimized as per requirement.
 #include <iostream>
 #include <math.h>
+#include <utility> 
+#include "newton-rapston-method.cpp"
+using namespace std;
 float objfun(float x, float y)
 {
     float z;
-    z = pow(x, 2) + pow(y * 2) + 2 * x + 5;
+    z = pow(x, 2) + pow(y,2) + 2 * x + 5;
     return z;
 }
-float partdelx(float x)
+float derivativeobj(float x, float y)
+{
+    float z;
+    z = 2 * x + 2 * y + 2;
+    return z;
+}
+float partdelx(float x, float y)
 {
     float delz_wrtx;
     delz_wrtx = 2 * x + 2;
     return delz_wrtx;
 }
-float partdely(float y)
+float partdely(float x, float y)
 {
     float delz_wrty;
     delz_wrty = 2 * y;
     return delz_wrty;
 }
+
+int nrmethod()
+{
+
+}
+
 float main()
 {
     float set[2];
     // give initial value.
-    set[0] = 2;//let
-    set[1] = 1; // let 
+cout<<"give initial guess coordinates.";
+cin>> set[0] >> set[1];
     // therefore our initial value is (x,y)=(2,1)
     //now we will evaluate partial derivate at these points.
-    float delz_wrtx,delz_wrty;
-    delz_wrtx = partdelx(objfun(set[0],set[1]));
-    delz_wrty = partdely(objfun(set[0],set[1]));
-    float delz[2];
-    delz[0] = delz_wrtx;
-    delz[1] = delz_wrty;
-    float h;
-    
+    float delz_wrtx, delz_wrty, value[50][2];
+    float delf[2], h = 0;
+    delz_wrtx = partdelx(set[0], set[1]);
+    delz_wrty = partdely(set[0], set[1]);
+    delf[0] = delz_wrtx;
+    delf[1] = delz_wrty;
+    int i, j;
+    float nextvalue[2];
+    for (j = 0; j < 50; j++)
+    {
+        for (i = 0; i < 2; i++)
+        {
+            nextvalue[i] = set[i] + (h * delf[i]);
+            value[j][i] = nextvalue[i];  
+        }
+    }
+}
 }
