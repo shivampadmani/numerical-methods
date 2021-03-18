55f9c9769d2ff3d63465a8899c4b3cd008d1f30f
shivampadmani <padmanishivamb@gmail.com>
2021-03-18 07:49:35 +0530

    differentiation is now possible

--- generalized-reduced-problem-method.cpp
+++ generalized-reduced-problem-method.cpp
@@ -1,81 +1,45 @@
 // GRG Numerical Technique
 // Here we will primarily make code for non linear function with two variables only.Later it will be optimized as per requirement.
 #include <iostream>
+using namespace std;
 #include <math.h>
-#include <utility> 
+#define n 50
 #include "forward.hpp"
 using namespace autodiff;
-/*float objfun(float x, float y)
-{
-    float z;
-    z = pow(x, 2) + pow(y,2) + 2 * x + 5;
-    return z;
-}
-float objfunh(float x, float y)
-{
-    float z;
-    z = pow(x+h*y,2) + pow(x+h*y,2) + 2 * (x+h*y) + 5;
-    return z;
-}
-float derivativeobj(float x, float y)
-{
-    float z;
-    z = 2 * x + 2 * y + 2;
-    return z;
-}
-float partdelx(float x, float y)
-{
-    float delz_wrtx;
-    delz_wrtx = 2 * x + 2;
-    return delz_wrtx;
-}
-float partdely(float x, float y)
-{
-    float delz_wrty;
-    delz_wrty = 2 * y;
-    return delz_wrty;
-}
-
-int nrmethod()
+dual f(dual x, dual y)
 {
 
+    return pow(x, 2) + pow(y, 2) + 2 * x + 4;
 }
-*/
 int main()
 {
     
-    /*float set[2];
-    // give initial value.
-cout<<"give initial guess coordinates.";
-//cin>> set[0] >> set[1];
-set[0]=2;
-set[1]=1;//assumed value as initial.
-    // therefore our initial value is (x,y)=(2,1)
-    //now we will evaluate partial derivate at these points.
-    float delz_wrtx, delz_wrty, value[50][2];
-    float delf[2], h = 0;
-    delz_wrtx = partdelx(set[0], set[1]);
-    delz_wrty = partdely(set[0], set[1]);
-    delf[0] = delz_wrtx;
-    delf[1] = delz_wrty;
-    int i, j;
-    float nextvalue[2];
+    //Suppose initial value first
+    dual x = 2.0; // the input variable x
+    dual y = 1.0; // the input variable y
+    dual u = f(x, y);//call function
+    int i,j;
+    dual val[n][2];
+    val[0][0] = x;
+    val[0][1] = y;
+cout<<"First initialized value are : \n" <<endl;
+    cout <<"x0 = "<< val[0][0] << endl;
+    cout <<"y0 = "<< val[0][1] << endl;
+
     
-    for (j = 0; j < 50; j++)
-    {
-       
-            pair<float,float> PAIR1;
-            PAIR1.first = set[0];
-            PAIR1.second = delf[0];
-            objfunh(PAIR1.first,PAIR1.second)
-             pair<float,float> PAIR2;
-            PAIR2.first = set[0];
-            PAIR2.second = delf[0];
-            objfunh(PAIR2.first,PAIR2.second)
-            /*nextvalue[i] = set[i] + (h * delf[i]);
-            value[j][i] = nextvalue[i];  
-        
-    }
-}*/
+for (i=0;i<1;i++){      //outer loop to iterate our values
+//cout<<"\n Next value"<<val[i][1]<<" , " << val[i][2] <<endl;
+}
+  
+    double    p = (double)val[i][0];
+double q = (double)val[i][1];
+    double dudx = derivative(f, wrt(x), at(x, y));
+    double dudy = derivative(f, wrt(y), at(x, y));
+
+    cout << "u = " << u << endl;        // print the evaluated output u
+    cout << "du/dx = " << dudx << endl; // print the evaluated derivative du/dx
+    cout << "du/dy = " << dudy << endl; // print the evaluated derivative du/dx
+    //to calculate gradient, evaluate partial derivaties at those point.
+    double delu[2] = {dudx,dudy};
 
 }
